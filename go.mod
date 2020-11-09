cd33b3e6813ca8861830bc43f8cbfc17f21c7822
AoiHosizora <a970335605@hotmail.com>
2020-11-09 19:41:59 +0800

    feat(decode): implement decode using lzstring

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Aoi-hosizora/manhuagui-backend
 
 go 1.15
+
+require github.com/PuerkitoBio/goquery v1.6.0
--- main.go
+++ main.go
@@ -1,16 +1,104 @@
 package main
 
 import (
-	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"log"
+	"net/http"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	fmt.Println("hw")
+	req, err := http.NewRequest("GET", "https://www.manhuagui.com/comic/34707/472931.html", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	body := resp.Body
+	defer body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var script *goquery.Selection
+	doc.Find("script").Each(func(i int, sel *goquery.Selection) {
+		_, ok := sel.Attr("src")
+		if !ok && strings.Contains(sel.Text(), `window["\x65\x76\x61\x6c"]`) {
+			script = sel
+		}
+	})
+	if script == nil {
+		log.Fatalln("could not find decode script")
+	}
+	decodeScript := script.Text()
+	found := regexp.MustCompile(`;return p;}\('(.+\(\);)',(.+?),(.+),'(.+)'\['\\x73`).FindAllStringSubmatch(decodeScript, 1)
+	if len(found) == 0 {
+		log.Fatalln("could not find decode text")
+	}
+	p := found[0][1]                  // XX.YY({...}).ZZ();
+	a, _ := strconv.Atoi(found[0][2]) // 00
+	c, _ := strconv.Atoi(found[0][3]) // 00
+	k := found[0][4]                  // ~['\x73\x70\x6c\x69\x63']('\x7c')
+
+	log.Println(p)
+	log.Println(a)
+	log.Println(c)
+	log.Println(k)
+
+	log.Println(decode(p, a, c, k))
+}
+
+func decode(p string, a, c int, k string) string {
+	k, err := DecompressFromEncodedUriComponent(k)
+	if err != nil {
+		return ""
+	}
+	ks := strings.Split(k, "|")
+
+	var e func(c int) string
+	e = func(c int) string {
+		p1 := ""
+		if c < a {
+			p1 = ""
+		} else {
+			p1 = e(c / a)
+		}
+		p2 := byte(0)
+		c = c % a
+		if c > 35 {
+			p2 = byte(c + 29)
+		} else {
+			p2 = "0123456789abcdefghijklmnopqrstuvwxyz"[c]
+		}
+		return p1 + string(p2)
+	}
+
+	d := map[string]string{}
+	for c--; c > 0; c-- {
+		if len(ks) <= c || ks[c] == "" {
+			d[e(c)] = e(c)
+		} else {
+			d[e(c)] = ks[c]
+		}
+	}
+	p = regexp.MustCompile(`\b\w\b`).ReplaceAllStringFunc(p, func(s string) string {
+		return d[s]
+	})
+	return p
 }
 
 // https://github.com/Aoi-hosizora/recent_works/issues/57
 
 /*
+// https://www.manhuagui.com/comic/34707/472931.html
+
 <script type="text/javascript" src="https://cf.hamreus.com/scripts/core_5649DEA1194321210A9977D8C49E535033C95CAA.js"></script>
 <script type="text/javascript">
 	window["\x65\x76\x61\x6c"](
@@ -29,6 +117,8 @@ func main() {
 			while (c--) {
 				if (k[c]) {
 					p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c]);
+					// =>
+					p = p.replace(/\b\w\b/g, function (e) { return d[e] });
 				}
 			}
 			return p;
@@ -43,4 +133,16 @@ func main() {
 		);
 	);
 </script>
+*/
+
+/*
+
+'D41hWAODmwO4FMBGliBpvAjMDhd6JAZgBYB2ABmOEgCcEBJAOwEsAXYRgW2gBEBDZn4ADNGTAM4ALBABNspIYwA2CUcADG9HuwQgSAJgCc+LKsYyUjVcEAAcoDh0q4Eag4Eg1anp4EvrYsgngtHakKL4wABCcACiPADiAIoAWgByjAD60ACCAML0ABwAEnkACroA8gCOsdgAbKQArIa1ORi6wKIKlDQAbrQyevrELfQIAB7MPZR84sAAygCyeU4KAPaqANYpqpai/MwArioYohhT+voYGEA='
+.splic('|')
+
+LZString.decompressFromBase64(
+	'D41hWAODmwO4FMBGliBpvAjMDhd6JAZgBYB2ABmOEgCcEBJAOwEsAXYRgW2gBEBDZn4ADNGTAM4ALBABNspIYwA2CUcADG9HuwQgSAJgCc+LKsYyUjVcEAAcoDh0q4Eag4Eg1anp4EvrYsgngtHakKL4wABCcACiPADiAIoAWgByjAD60ACCAML0ABwAEnkACroA8gCOsdgAbKQArIa1ORi6wKIKlDQAbrQyevrELfQIAB7MPZR84sAAygCyeU4KAPaqANYpqpai/MwArioYohhT+voYGEA='
+).split('|')
+'||||||||||jpg|webp|第1|1话||34707|preInit|imgData|finished|10|files|cname||472931|cid|bpic|一霎一花|bname|bid|len|11|false|ps3|BwEaGQZNi_gACn8HHP2OqQ|1605935812|sl|prevId|472972|nextId|path|SMH|block_cc|status|1s1h|9911'
+
 */
